88630d7be2246b619f87866ed7b5cb805388cb0f
martinyde <yde001@gmail.com>
2025-01-29 12:15:09 +0100

    Added default local db, migrations and entities

---
+++ src/Entity/Url.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UrlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UrlRepository::class)]
+class Url
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shortUri = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\ManyToOne(inversedBy: 'urls')]
+    private ?Qr $qr = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShortUri(): ?string
+    {
+        return $this->shortUri;
+    }
+
+    public function setShortUri(string $shortUri): static
+    {
+        $this->shortUri = $shortUri;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getQr(): ?Qr
+    {
+        return $this->qr;
+    }
+
+    public function setQr(?Qr $qr): static
+    {
+        $this->qr = $qr;
+
+        return $this;
+    }
+}
