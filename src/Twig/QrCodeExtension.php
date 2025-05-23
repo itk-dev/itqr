76e0c00b3c8f7c284dcbf1dbf820dc14d3f58b87
Jeppe Krogh <kjej@aarhus.dk>
2025-05-23 15:39:14 +0200

    Added twig extension to inject functions to generate required classes for qr preview params

---
+++ src/Twig/QrCodeExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig;
+
+use App\Helper\DownloadHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class QrCodeExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly DownloadHelper $downloadHelper
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('create_color_from_hex', [$this->downloadHelper, 'createColorFromHex']),
+            new TwigFunction('create_label_margin', [$this->downloadHelper, 'createLabelMargin']),
+            new TwigFunction('create_font_interface', [$this->downloadHelper, 'createFontInterface']),
+
+        ];
+    }
+}
