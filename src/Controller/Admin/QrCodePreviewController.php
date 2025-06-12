ff43f475c6075a6da4be1875e8710c7676c1c290
Jeppe Krogh <kjej@aarhus.dk>
2025-06-12 09:57:43 +0200

    Moved qr preview generation from controller into service

--- src/Controller/Admin/QrCodePreviewController.php
+++ src/Controller/Admin/QrCodePreviewController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\DTO\DownloadSettingsDTO;
 use App\Helper\DownloadHelper;
 use App\Repository\QrRepository;
 use App\Repository\QrVisualConfigRepository;
+use App\Service\QrCodePreviewService;
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
@@ -22,6 +24,7 @@ readonly class QrCodePreviewController
         private DownloadHelper $downloadHelper,
         private QrRepository $qrRepository,
         private QrVisualConfigRepository $qrVisualConfigRepository,
+        private QrCodePreviewService $qrCodePreviewService,
     ) {
     }
 
@@ -36,136 +39,13 @@ readonly class QrCodePreviewController
      * @return JsonResponse a JSON response containing the generated QR codes as a base64-encoded array
      *
      * @throws ValidationException
+     * @throws \Exception
      */
     #[Route('/admin/generate-qr-codes', name: 'admin_generate_qr_codes', methods: ['POST'])]
     public function generateQrCode(Request $request): JsonResponse
     {
-        // Extract data from the request
-        $data = $request->request->all();
-        $formName = $data['formName'];
-        $downloadSettings = $data[$formName] ?? [];
-
-        $selectedQrCodes = $data['selectedQrCodes'] ?? [];
-
-        // Can be defined as such to prompt a qr example preview.
-        if ('examplePreview' !== $selectedQrCodes) {
-            $selectedQrCodes = json_decode($selectedQrCodes, true);
-        }
-
-        /*
-        Extract logo from the request (When a new image is uploaded either from batch download page or design config page.
-        ImageField places uploaded files in ['logo']['file']
-        FileType places uploaded files in ['logo']
-        */
-        $logo = null;
-        $formData = $request->files->get($formName);
-
-        if ($formData && isset($formData['logo'])) {
-            if ($formData['logo'] instanceof UploadedFile) {
-                $logo = $formData['logo'];
-            } elseif (isset($formData['logo']['file']) && $formData['logo']['file'] instanceof UploadedFile) {
-                $logo = $formData['logo']['file'];
-            }
-        }
-
-        // If a design is edited, it contains an id from where we can grab the entity.
-        $entity = isset($downloadSettings['id']) && !$logo ? $this->qrVisualConfigRepository->findOneBy(['id' => $downloadSettings['id']]) : null;
-
-        // Uploaded logo > logo from entity > logo from logoPath (only on batch download page)
-        if ($entity && $entity->getLogo()) {
-            $downloadSettings['logoPath'] = $_ENV['APP_BASE_UPLOAD_PATH'].$entity->getLogo();
-            $logo = $downloadSettings['logoPath'];
-        } elseif (isset($downloadSettings['logoPath']) && !$logo instanceof UploadedFile) {
-            $logo = $downloadSettings['logoPath'];
-        }
-
-        $size = (int) ($downloadSettings['size'] ?? 400);
-        $margin = (int) ($downloadSettings['margin'] ?? 0);
-        $backgroundColor = $downloadSettings['backgroundColor'] ?? '#ffffff';
-        $backgroundColor = $this->downloadHelper->createColorFromHex($backgroundColor);
-        $foregroundColor = $downloadSettings['foregroundColor'] ?? '#000000';
-        $foregroundColor = $this->downloadHelper->createColorFromHex($foregroundColor);
-        $labelText = $downloadSettings['labelText'] ?? '';
-        $labelFont = $this->downloadHelper->createFontInterface((int) ($downloadSettings['labelSize'] ?? 12));
-        $labelTextColor = $downloadSettings['labelTextColor'] ?? '#000000';
-        $labelTextColor = $this->downloadHelper->createColorFromHex($labelTextColor);
-        $labelMargin = new Margin((int) ($downloadSettings['labelMarginTop'] ?? 0), 0, (int) ($downloadSettings['labelMarginBottom'] ?? 0), 0);
-        $errorCorrectionLevel = [
-            'low' => ErrorCorrectionLevel::Low,
-            'medium' => ErrorCorrectionLevel::Medium,
-            'quartile' => ErrorCorrectionLevel::Quartile,
-            'high' => ErrorCorrectionLevel::High,
-        ][$downloadSettings['errorCorrectionLevel'] ?? 'medium'] ?? ErrorCorrectionLevel::Medium;
-
-        // Initialize the array for storing base64-encoded QR codes
-        $data = [];
-
-        if ('examplePreview' === $selectedQrCodes) {
-            // Generate the QR Code using Endroid QR Code Builder
-            $builder = new Builder();
-            $result = $builder->build(
-                data: 'qr visual config example preview',
-                encoding: new Encoding('UTF-8'),
-                errorCorrectionLevel: $errorCorrectionLevel,
-                size: $size,
-                margin: $margin,
-                foregroundColor: $foregroundColor,
-                backgroundColor: $backgroundColor,
-                labelText: $labelText,
-                labelFont: $labelFont,
-                labelAlignment: LabelAlignment::Center,
-                labelMargin: $labelMargin,
-                labelTextColor: $labelTextColor,
-                logoPath: $logo,
-                logoPunchoutBackground: false,
-            );
-
-            // Convert the QR code image to base64 and add to the array
-            $data['examplePreview'] = 'data:image/png;base64,'.base64_encode($result->getString());
-
-            // Respond with the array of QR codes as base64-encoded PNGs
-            return new JsonResponse([
-                'qrCodes' => $data,
-            ]);
-        }
-
-        // Loop through each selected QR code entity ID
-        foreach ($selectedQrCodes as $qrCodeId) {
-            // Replace this with logic to retrieve the URL (or string) for each QR code entity
-            $qrCodeUrl = $this->qrRepository->findOneBy(['id' => $qrCodeId])->getUrls()->first()->getUrl();
-            $qrCodeTitle = $this->qrRepository->findOneBy(['id' => $qrCodeId])->getTitle();
-
-            if (!$qrCodeUrl) {
-                continue;
-            }
-
-            // Generate the QR Code using Endroid QR Code Builder
-            $builder = new Builder();
-            $result = $builder->build(
-                data: $qrCodeUrl,
-                encoding: new Encoding('UTF-8'),
-                errorCorrectionLevel: $errorCorrectionLevel,
-                size: $size,
-                margin: $margin,
-                foregroundColor: $foregroundColor,
-                backgroundColor: $backgroundColor,
-                labelText: $labelText,
-                labelFont: $labelFont,
-                labelAlignment: LabelAlignment::Center,
-                labelMargin: $labelMargin,
-                labelTextColor: $labelTextColor,
-                logoPath: $logo,
-                logoPunchoutBackground: false,
-            );
-
-            // Convert the QR code image to base64 and add to the array
-            $data[$qrCodeTitle] = 'data:image/png;base64,'.base64_encode($result->getString());
-        }
-
-        // Respond with the array of QR codes as base64-encoded PNGs
-        return new JsonResponse([
-            'qrCodes' => $data,
-        ]);
+        // Send request data to service to handle generation
+        return $this->qrCodePreviewService->generateQrCode($request);
     }
 
     /**
