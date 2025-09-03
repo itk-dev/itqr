1dbc1d5f0cf874ced0aec1c6400b4f1344f25493
Jeppe Krogh <kjej@aarhus.dk>
2025-09-03 11:56:18 +0200

    Added Helper class for settings alternativeUrl on QR

--- src/Helper/QrHelper.php
+++ src/Helper/QrHelper.php
@@ -6,6 +6,8 @@ namespace App\Helper;
 use App\Entity\Tenant\Qr;
 use App\Enum\QrStatusEnum;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class QrHelper
 {
@@ -19,11 +21,16 @@ class QrHelper
      * Change the status of a QR entity.
      * @throws \Exception
      */
-    public function archive(Qr $qrEntity): Qr
+    public function archive(Qr $qrEntity, ?string $alternativeUrl): Response
     {
+        if ($alternativeUrl) {
+            $qrEntity->setAlternativeUrl($alternativeUrl);
+        }
         $qrEntity->setStatus(QrStatusEnum::ARCHIVED);
         $this->entityManager->flush();
 
-        return $qrEntity;
+        return new JsonResponse([
+            'message' => 'QR code archived successfully' . ($alternativeUrl ? " with alternative URL: $alternativeUrl" : '')
+        ]);
     }
 }
