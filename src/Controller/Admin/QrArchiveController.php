fb11f7286ce8bd94855b66bd8e6a7bb99cb5b0b3
Jeppe Krogh <kjej@aarhus.dk>
2025-09-05 10:33:54 +0200

    Added archive method to QrArchiveController

--- src/Controller/Admin/QrArchiveController.php
+++ src/Controller/Admin/QrArchiveController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Form\Type\QrArchiveType;
 use App\Helper\QrHelper;
 use App\Repository\QrRepository;
-use GuzzleHttp\Utils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,7 +27,7 @@ final class QrArchiveController extends AbstractController
      * @throws \Exception
      */
     #[Route('/admin/archive', name: 'admin_qr_archive')]
-    public function index(int $id): Response
+    public function archive(int $id): Response
     {
         $form = $this->createForm(QrArchiveType::class);
         $request = $this->requestStack->getCurrentRequest();
@@ -60,6 +59,27 @@ final class QrArchiveController extends AbstractController
 
         return $this->render('form/qrArchive.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/unarchive', name: 'admin_qr_unarchive')]
+    public function unarchive(int $id): Response
+    {
+        $qrEntity = $this->qrRepository->find($id);
+        if (!$qrEntity) {
+            throw $this->createNotFoundException('No QR code found');
+        }
+        $response = $this->qrHelper->unarchive($qrEntity);
+
+        $message = json_decode($response->getContent(), true)['message'];
+        $this->addFlash('success', $message);
+        return $this->redirectToRoute('qr_index', [
+            'filters' => [
+                'status' => [
+                    'comparison' => '=',
+                    'value' => 'ACTIVE'
+                ]
+            ]
         ]);
     }
 }
